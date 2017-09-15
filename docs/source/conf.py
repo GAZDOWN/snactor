79c9281adab5bd2e39a7da073d16d803bcda1fb3
GAZDOWN <mgazdik@redhat.com>
2017-09-15 14:05:53 +0200

    removed mockup files, updated makefile

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -133,7 +133,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Leapp.tex', 'Leapp Documentation',
+    (master_doc, 'Leapp-snactor.tex', 'Leapp-Snactor Documentation',
      'Leapp Team', 'manual'),
 ]
 
@@ -143,7 +143,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'leapp', 'Leapp Documentation',
+    (master_doc, 'leapp-Snactor', 'Leapp-Snactor Documentation',
      [author], 1)
 ]
 
@@ -154,7 +154,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Leapp', 'Leapp Documentation',
+    (master_doc, 'Leapp-Snactor', 'Leapp-Snactor Documentation',
      author, 'Leapp', 'One line description of project.',
      'Miscellaneous'),
 ]
